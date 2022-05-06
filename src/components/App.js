b35d68c0a8bd66aebefa9f0112e42c91af05d181
Kyrian <dr.kyrian.onwe@gmail.com>
2022-05-06 15:19:33 -0500

    commit tree b0a0a16af0aa8ecd77fbad61be28904a284f2220

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+
+  const [listings, setListings] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:6001/listings")
+      .then((r) => r.json())
+      .then((data) => setListings(data))
+
+  }, [])
+  console.log(listings);
+
+  function handleDelete(id){
+    fetch(`http://localhost:6001/listings/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      } ,
+    })
+      .then((resp) => resp.json())
+      .then((data) => setListings(listings.filter((listing) => listing.id !== id )))
+  }
+
+  function handleFilter(event){
+    event ? setListings(listings) : setListings(listings.filter((listing) => listing.description === event.target.value ))
+  }
+
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header handleFilter={handleFilter} />
+      <ListingsContainer data={listings} deleteCard={handleDelete} />
     </div>
   );
 }
--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,16 @@
 import React from "react";
-// import ListingCard from "./ListingCard";
+import ListingCard from "./ListingCard";
 
-function ListingsContainer() {
+function ListingsContainer(props) {
+  const pack = (props.data)
+  // console.log(props.deleteCard)
+
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {/* use the ListingCard component to display listings */
+        pack.map((p) => <ListingCard id={p.id} description={p.description} img={p.image} location={p.location} key={p.id} delCard={props.deleteCard} />
+        )}
       </ul>
     </main>
   );
